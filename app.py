e9fe776dddcb8feaacd8bbc86827aaa5784766c5
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-09-24 02:26:38 -0600

    api completed

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import openai
 app = Flask(__name__, template_folder='./template')
 app.config['SECRET_KEY'] = 'asdasdasas'
 app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
-openai.api_key = "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"
+openai.api_key = "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"
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
@@ -29,22 +29,18 @@ def get_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
     form = UploadForm()
-    response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt="I want to make a list of recipes with the next ingredients (you don't have to include every single ingredient in every single recipe)\n\n- sauce\n- cabbage\n- orange\n- pear\n- avocado\n- apple\n- juice\n- milk\n- bread",
-            temperature=0.3,
-            max_tokens=60,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            stop=["\n"]
-        )
-    predicted_text = response.choices[0].text
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = [
+            {"role": "user", "content": "Hello, how are you?"}
+        ]
+    )
+
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
         file_url = url_for('get_file', filename=filename)
-        
-    return render_template('index.html', form=form, file_url=file_url, predicted_text=predicted_text)
+
+    return render_template('index.html', form=form, response=response)
 
 if __name__ == '__main__':
     app.run(debug=True)
